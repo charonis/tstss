0aab55ca0a4b8677d4e124557ee09f00dd40df80
charon <you@example.com>
2024-10-09 01:27:16 +0300

    modified:   config.py

--- config.py
+++ config.py
@@ -1,25 +1,11 @@
 from pydantic_settings import SettingsConfigDict, BaseSettings
 
 class Settings(BaseSettings):
-    
-    DB_HOST: str
-    DB_PORT: int
-    DB_NAME: str
-    DB_USER:str
-    DB_PASSWORD:str
     KEY_ID: str
     ACCESS_KEY: str
-    
-    @property
-    def DATABASE_URL_ASYNC_PSYCOPG(self):
-        
-        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
-        
     
     
     model_config = SettingsConfigDict(env_file=".env")
         
 settings = Settings()    
 
-# print(settings.KEY_ID)
-# print(settings.ACCESS_KEY)
